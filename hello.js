17b9d471776322d75c87a11753d0b8b43aecbf07
Mohamed Mazy <mazymohamed06@gmail.com>
2020-11-17 14:43:56 +0000

    types of var

--- hello.js
+++ hello.js
@@ -1,1 +1,11 @@
 alert('Hello world!');
+var number = 2;
+alert(typeof number); // print : « number »
+
+var text = 'Mon texte';
+alert(typeof text); // print : « string »
+
+var aBoolean = false;
+alert(typeof aBoolean); // print : « boolean »
+
+alert(typeof nothing); // print : « undefined »
