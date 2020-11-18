ccfc9bd9e4789937245d2a01d1082ae1c5af9bc9
Mohamed Mazy <mazymohamed06@gmail.com>
2020-11-18 15:01:47 +0000

    Covert string to int

--- hello.js
+++ hello.js
@@ -1,4 +1,6 @@
 alert('Hello world!');
+
+// Types of var
 var number = 2;
 alert(typeof number); // print : « number »
 
@@ -10,17 +12,28 @@ alert(typeof aBoolean); // print : « boolean »
 
 alert(typeof nothing); // print : « undefined »
 
+// Calculation
 var number1 = 3, number2 = 2, result;
 result = number1 * number2;
 alert(result); // print : « 6 »
 
+// Concatenation
 var text = 'Bonjour ';
 text += 'toi';
 alert(text); // print « Bonjour toi ».
 
-
+//Interaction with user
 var start = 'Bonjour ', name, end = ' !', result;
 
 name   = prompt('Quel est votre prénom ?');
 result = start + name + end;
+alert(result);
+
+// Covert to other type
+var first, second, result;
+
+first = prompt('Entrez le premier chiffre :');
+second = prompt('Entrez le second chiffre :');
+result = parseInt(first) + parseInt(second);
+
 alert(result);
