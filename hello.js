036883aab0365698be09072875db399fe7255038
Mohamed Mazy <mazymohamed06@gmail.com>
2020-11-18 14:54:52 +0000

    concatenation sting

--- hello.js
+++ hello.js
@@ -13,3 +13,7 @@ alert(typeof nothing); // print : « undefined »
 var number1 = 3, number2 = 2, result;
 result = number1 * number2;
 alert(result); // print : « 6 »
+
+var text = 'Bonjour ';
+text += 'toi';
+alert(text); // print « Bonjour toi ».
