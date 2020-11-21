ce4f10931fa743d64c49401658e8cf57f5c5370c
Mohamed Mazy <mazymohamed06@gmail.com>
2020-11-21 11:42:03 +0000

    add and delete (start and end of array)

--- array.js
+++ array.js
@@ -3,4 +3,16 @@
 var myArray_a = [42, 12, 6, 3];
 var myArray_b = [42, 'Sébastien', 12, 'Laurence'];
 
+// Print item
 alert(myArray_b[1]); // print : « Sébastien »
+
+// Change an item
+myArray_a[1] = 'Clarisse';
+
+myArray_a.push('Ludovic'); // add « Ludovic » end of array
+
+myArray_a.pop(); // delete « Ludovic » in the end of array
+
+myArray_a.unshift('Ludovic'); // add « Ludovic » beginning of array
+
+myArray_a.shift(); // delete « Ludovic » in the beginning of the array
